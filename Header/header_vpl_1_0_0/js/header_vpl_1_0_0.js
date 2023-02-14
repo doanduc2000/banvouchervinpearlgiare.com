9fd0c642e7d85ca5542ab5f83497be102dc356e2
doanduc2000 <doanminhducvip2000@gmail.com>
2023-02-14 16:38:42 +0700

    update

---
+++ Header/header_vpl_1_0_0/js/header_vpl_1_0_0.js
@@ -0,0 +1,15 @@
+document.getElementById("headerMenuBtn").addEventListener("click", () => {
+    document.getElementById("headerSideBar").classList.add("show");
+    document.getElementById("headerBg").style.display = "block";
+});
+document.getElementById("headerBg").addEventListener("click", () => {
+    document.getElementById("headerSideBar").classList.remove("show");
+    document.getElementById("headerBg").style.display = "none";
+});
+
+const menuItem = document.querySelectorAll(".header_dhh_2_0_0__item");
+menuItem.forEach((item) => {
+    item.addEventListener("click", () => {
+        item.classList.toggle("open");
+    });
+});
