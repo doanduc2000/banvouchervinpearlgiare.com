be18a483755a68be5a663ae651514826e9c61b13
doanduc2000 <doanminhducvip2000@gmail.com>
2023-02-21 15:53:49 +0700

    update

--- Home/voucher_vpl_1_0_0/js/voucher_vpl_1_0_0.js
+++ Home/voucher_vpl_1_0_0/js/voucher_vpl_1_0_0.js
@@ -3,31 +3,51 @@ const voucher_vpl_1_0_0Data = [{
     id: '1',
     name: 'voucher vinpearl nha trang',
     vouchers: [{
-        name: 'vinpearl nha trang bay',
-        price: '2.000.000',
-        link: '#',
-        img: `${voucher_vpl_1_0_0Url}images/voucher_vpl_1_0_0-pic1.jpg`
-    }, {
-        name: 'vinpearl nha trang bay',
-        price: '2.000.000',
-        link: '#',
-        img: `${voucher_vpl_1_0_0Url}images/voucher_vpl_1_0_0-pic2.jpg`
-    }, {
-        name: 'vinpearl nha trang bay',
-        price: '2.000.000',
-        link: '#',
-        img: `${voucher_vpl_1_0_0Url}images/voucher_vpl_1_0_0-pic1.jpg`
-    }, {
-        name: 'vinpearl nha trang bay',
-        price: '2.000.000',
-        link: '#',
-        img: `${voucher_vpl_1_0_0Url}images/voucher_vpl_1_0_0-pic2.jpg`
-    }, {
-        name: 'vinpearl nha trang bay',
-        price: '2.000.000',
-        link: '#',
-        img: `${voucher_vpl_1_0_0Url}images/voucher_vpl_1_0_0-pic1.jpg`
-    }]
+            name: 'vinpearl nha trang bay',
+            price: '2.000.000',
+            link: '#',
+            img: `${voucher_vpl_1_0_0Url}images/voucher_vpl_1_0_0-pic1.jpg`
+        }, {
+            name: 'vinpearl nha trang bay',
+            price: '2.000.000',
+            link: '#',
+            img: `${voucher_vpl_1_0_0Url}images/voucher_vpl_1_0_0-pic2.jpg`
+        }, {
+            name: 'vinpearl nha trang bay',
+            price: '2.000.000',
+            link: '#',
+            img: `${voucher_vpl_1_0_0Url}images/voucher_vpl_1_0_0-pic1.jpg`
+        }, {
+            name: 'vinpearl nha trang bay',
+            price: '2.000.000',
+            link: '#',
+            img: `${voucher_vpl_1_0_0Url}images/voucher_vpl_1_0_0-pic2.jpg`
+        },
+        {
+            name: 'vinpearl nha trang bay',
+            price: '2.000.000',
+            link: '#',
+            img: `${voucher_vpl_1_0_0Url}images/voucher_vpl_1_0_0-pic1.jpg`
+        },
+        {
+            name: 'vinpearl nha trang bay',
+            price: '2.000.000',
+            link: '#',
+            img: `${voucher_vpl_1_0_0Url}images/voucher_vpl_1_0_0-pic1.jpg`
+        },
+        {
+            name: 'vinpearl nha trang bay',
+            price: '2.000.000',
+            link: '#',
+            img: `${voucher_vpl_1_0_0Url}images/voucher_vpl_1_0_0-pic1.jpg`
+        },
+        {
+            name: 'vinpearl nha trang bay',
+            price: '2.000.000',
+            link: '#',
+            img: `${voucher_vpl_1_0_0Url}images/voucher_vpl_1_0_0-pic1.jpg`
+        },
+    ]
 }, {
     id: '2',
     name: 'voucher vinpearl phú quốc',
@@ -153,6 +173,7 @@ const voucher_vpl_1_0_0Temp = (data) => {
                 vouchers.push(data.vouchers[i])
             }
         }
+
         vouchers.forEach(item => {
             voucherItem += ` <a href="${item.link}" class="voucher_vpl_1_0_0__item">
             <div class="voucher_vpl_1_0_0__promo">Giá: ${item.price} VND/đêm</div>
@@ -164,22 +185,57 @@ const voucher_vpl_1_0_0Temp = (data) => {
         </div>
     </a>`
         })
+
         return `
         <div class="voucher_vpl_1_0_0__wrapper">
-        <div class="container">
-    <h2 class="voucher_vpl_1_0_0__title">${data.name}</h2>
-    <div class="voucher_vpl_1_0_0__box">
-      ${voucherItem}
-    </div>
-    ${data.vouchers.length > limit ? `<div class="voucher_vpl_1_0_0__viewMore"><button onclick="">Xem thêm </button></div>`:''}
-    </div>
-</div>`
+            <div class="container">
+                <h2 class="voucher_vpl_1_0_0__title">${data.name}</h2>
+                <div class="voucher_vpl_1_0_0__box">
+                    ${voucherItem}
+                </div>
+                ${data.vouchers.length > limit ? `<div class="voucher_vpl_1_0_0__viewMore"><button onclick="viewMore(${data.id})">Xem thêm </button></div>` : ''}
+            </div>
+        </div>`
 }
 
-const render=()=>{
-    document.getElementById('voucher_vpl_1_0_0__root').innerHTML=''
-for (let i = 0; i < voucher_vpl_1_0_0Data.length; i++) {
-   document.getElementById('voucher_vpl_1_0_0__root').insertAdjacentHTML("beforeend", voucher_vpl_1_0_0Temp(voucher_vpl_1_0_0Data[i]))
-}
+const render = () => {
+    document.getElementById('voucher_vpl_1_0_0__root').innerHTML = ''
+    for (let i = 0; i < voucher_vpl_1_0_0Data.length; i++) {
+        document.getElementById('voucher_vpl_1_0_0__root').insertAdjacentHTML("beforeend", voucher_vpl_1_0_0Temp(voucher_vpl_1_0_0Data[i]))
+    }
 }
 render()
+
+const voucher_vpl_1_0_0More = (data) => {
+    var vouchers = []
+    let voucherItem = ''
+    let limit = data.vouchers.length;
+   
+    if (data.vouchers.length <= limit) {
+        vouchers = data.vouchers
+    } else {
+        for (let i = 0; i < limit; i++) {
+            vouchers.push(data.vouchers[i])
+        }
+    }
+
+    vouchers.forEach(item => {
+        voucherItem += ` <a href="${item.link}" class="voucher_vpl_1_0_0__item">
+            <div class="voucher_vpl_1_0_0__promo">Giá: ${item.price} VND/đêm</div>
+        <div class="voucher_vpl_1_0_0__img">
+            <img width="219" height="148" src="${item.img}" alt="">
+        </div>
+        <div class="voucher_vpl_1_0_0__text"> 
+            <p>${item.name}</p>
+        </div>
+    </a>`
+    })
+    return voucherItem;    
+}
+
+function viewMore(index) {
+    let count = index - 1
+    document.getElementsByClassName('voucher_vpl_1_0_0__viewMore')[count].style.display='none';
+    document.getElementsByClassName('voucher_vpl_1_0_0__box')[count].innerHTML = '';
+    document.getElementsByClassName('voucher_vpl_1_0_0__box')[count].insertAdjacentHTML('beforeend', voucher_vpl_1_0_0More(voucher_vpl_1_0_0Data[count]))
+}
