a6fd2af8770bf1f57f389212a8b5643130702f34
doanduc2000 <doanminhducvip2000@gmail.com>
2023-02-22 16:52:40 +0700

    update

---
+++ Popup/popup_vpl_1_0_0/js/popup_vpl_1_0_0.js
@@ -0,0 +1,16 @@
+const popUp = document.getElementsByClassName('popup-btn')
+
+for (let i = 0; i < popUp.length; i++) {
+    popUp[i].addEventListener('click', () => {
+        document.getElementById('popup_vpl_1_0_0').style.display = 'flex'
+    })
+
+}
+window.onclick = function(e) {
+    if (e.target == document.getElementById('popup_vpl_1_0_0')) {
+        document.getElementById('popup_vpl_1_0_0').style.display = 'none'
+    }
+}
+document.getElementById('popup-close').addEventListener('click', () => {
+    document.getElementById('popup_vpl_1_0_0').style.display = 'none'
+})
